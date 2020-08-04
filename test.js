c687e4b648424af679198658093b4e71c872f72f
Grant Lu <grantlu@Grants-MBP.fios-router.home>
2020-08-04 14:32:13 -0400

    Test Xvfb

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-console.log("success")
+const { Builder } = require('selenium-webdriver')
+
+async function start() {
+  const chrome = require('selenium-webdriver/chrome')
+  const options = new chrome.Options()
+
+  options.addArguments('--disable-dev-shm-usage')
+  options.addArguments('--no-sandbox')
+
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+
+  await driver.get('https://google.com')
+  await driver.sleep(1000)
+  const text = await driver.executeScript('return document.documentElement.innerText')
+  console.log(text)
+  driver.quit()
+}
+
+start()
