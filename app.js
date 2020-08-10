6154f3d18dedfabdaa6128b6f660e3081e74d161
Grant Lu <grantlu@Grants-MBP.fios-router.home>
2020-08-10 10:27:44 -0400

    Adding express

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 2200; //picked arbitrary #
+​
 const { Builder, By, Key, util, until } = require("selenium-webdriver");
 var numeral = require('numeral');
 const chrome = require('selenium-webdriver/chrome')
@@ -5,17 +9,43 @@ const options = new chrome.Options()
 options.addArguments('--disable-dev-shm-usage')
 options.addArguments('--no-sandbox')
 options.addArguments('--headless')
-
+​
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+​
+​
+//const cp = require('child_process');
+​
+var link = "Null";
+​
+app.post("/NemoText",(req,res) => {
+    var user_data = req.body["specifics"];  //what we get from Nemobot user (the payload)
+    var link = main(user_data); // call main(user_data)
+   // var link = cp.fork("./home/grant/programs/portfolio/HeadlessSearch.js", user_data); //Not 100% sure about the directory
+   res.end();
+});
+​
+app.get("/search",(req,res)=>{
+    res.render('search.ejs',{data:link}); //not sure that's the data we want
+});
+​
+​
+​
+​
+​
+​
+​
 const driver = new Builder()
   .forBrowser('chrome')
   .setChromeOptions(options)
   .build()
-
+​
 let urls = []
 let coefficient = [] // (likes/views) * (likes/ (likes + dislikes))
 let bestvideo = ''
-
-
+​
+​
 async function ytsearch(topic, length) {
   let url = 'https://www.youtube.com/results?search_query=' + topic + ', ' + length;
   await driver.get(url)
@@ -23,9 +53,9 @@ async function ytsearch(topic, length) {
   await computeCoefficient()
   await getBestVid()
   driver.quit()
-
+​
 }
-
+​
 async function grabUrls() {
   driver.wait(until.elementLocated(By.id('video-title', 1000)))
   let alllink = await (await driver).findElements(By.id('video-title'))
@@ -33,13 +63,13 @@ async function grabUrls() {
     let url = await ele.getAttribute('href')
     try {
       urls.push(url.toString())
-
+​
     } catch (error) {
       continue
     }
   }
 }
-
+​
 async function computeCoefficient() {
   for (let i = 0; i < 3; i++) {
     let driver1 = new Builder()
@@ -58,16 +88,16 @@ async function computeCoefficient() {
     coefficient[i] = qualitycoefficient
     await (await driver1).quit()
   }
-
+​
 }
-
+​
 async function getBestVid() {
   var indexOfMaxValue = coefficient.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
   bestvideo = urls[indexOfMaxValue]
   console.log(bestvideo)
 }
-
-
+​
+​
 async function main(inputArr) {
   let maintopic = inputArr[0]
   let subtopic = inputArr[1]
@@ -78,3 +108,6 @@ async function main(inputArr) {
   // let topic = args.slice(0, -1).join(" ")
   ytsearch(topic, vidlength)
 } 
+​
+​
+module.exports = app;
