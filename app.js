64c3e0833116df788770eb2450691c3f5866fa75
Grant Lu <grantlu@Grants-MBP.fios-router.home>
2020-08-11 10:24:40 -0400

    change input method

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 2200; //picked arbitrary #
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 const { Builder, By, Key, util, until } = require("selenium-webdriver");
 var numeral = require('numeral');
 const chrome = require('selenium-webdriver/chrome')
@@ -18,24 +18,24 @@ var bool = false;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set("view options", {layout: false});
+app.set("view options", { layout: false });
 app.use('/', express.static(__dirname));
 app.use(bodyParser.json());
 
-app.post("/NemoText",(req,res) => {
-    var user_data = req.body["specifics"];  //what we get from Nemobot user (the payload)
-    // link = req.body["specifics"];
-    // console.log(link); // test the passed value
-    // user_data = ['precalculus', 'overview', 'long'];
-    link = main(user_data); // call main(user_data)
-    bool = true
-   // var link = cp.fork("./home/grant/programs/portfolio/HeadlessSearch.js", user_data); //Not 100% sure about the directory
-   res.end();
+app.post("/NemoText", (req, res) => {
+  var user_data = req.body["specifics"];  //what we get from Nemobot user (the payload)
+  // link = req.body["specifics"];
+  // console.log(link); // test the passed value
+  // user_data = ['precalculus', 'overview', 'long'];
+  link = main(user_data); // call main(user_data)
+  bool = true
+  // var link = cp.fork("./home/grant/programs/portfolio/HeadlessSearch.js", user_data); //Not 100% sure about the directory
+  res.end();
 });
 
 // if (bool) {
-app.get("/search",(req,res)=>{
-  res.render('search.ejs',{data:link}); //not sure that's the data we want
+app.get("/search", (req, res) => {
+  res.render('search.ejs', { data: link }); //not sure that's the data we want
 });
 // }
 // ​​
@@ -45,12 +45,12 @@ const driver = new Builder()
   .forBrowser('chrome')
   .setChromeOptions(options)
   .build()
-​
+
 let urls = []
 let coefficient = [] // (likes/views) * (likes/ (likes + dislikes))
 let bestvideo = ''
-​
-​
+
+
 async function ytsearch(topic, length) {
   let url = 'https://www.youtube.com/results?search_query=' + topic + ', ' + length;
   await driver.get(url)
@@ -58,9 +58,9 @@ async function ytsearch(topic, length) {
   await computeCoefficient()
   await getBestVid()
   driver.quit()
-​
+
 }
-​
+
 async function grabUrls() {
   driver.wait(until.elementLocated(By.id('video-title', 1000)))
   let alllink = await (await driver).findElements(By.id('video-title'))
@@ -68,13 +68,13 @@ async function grabUrls() {
     let url = await ele.getAttribute('href')
     try {
       urls.push(url.toString())
-​
+
     } catch (error) {
       continue
     }
   }
 }
-​
+
 async function computeCoefficient() {
   for (let i = 0; i < 3; i++) {
     let driver1 = new Builder()
@@ -93,28 +93,31 @@ async function computeCoefficient() {
     coefficient[i] = qualitycoefficient
     await (await driver1).quit()
   }
-​
+
 }
-​
+
 async function getBestVid() {
   var indexOfMaxValue = coefficient.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
   bestvideo = urls[indexOfMaxValue]
   console.log(bestvideo)
 }
-​
-​
-async function main(inputArr) {
-  let maintopic = inputArr[0]
-  let subtopic = inputArr[1]
-  let vidlength = inputArr[2]
-  let topic = maintopic + " " + subtopic
+
+
+async function main(input) {
+  let topic = input.substring(0, input.lastIndexOf(" ")).trim();
+  let vidlength = input.substring(input.lastIndexOf(" ") + 1).trim();
+  
+  // let maintopic = inputArr[0]
+  // let subtopic = inputArr[1]
+  // let vidlength = inputArr[2]
+  // let topic = maintopic + " " + subtopic
   // var args = process.argv.slice(2);
   // let vidlength = args[args.length - 1].trim()
   // let topic = args.slice(0, -1).join(" ")
   ytsearch(topic, vidlength)
-} 
-​
-​
+}
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
